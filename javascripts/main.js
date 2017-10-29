a62e0cf5970d9ffbafe7f79bae90d0c46af1076b
angus-c <anguscroll@gmail.com>
2017-10-29 14:36:24 -0700

    size

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import leftPad from 'just-left-pad';
 
-ReactDOM.render(<App/>, document.querySelector('#react-root'));
+ReactDOM.render(
+  <App x={leftPad('angus', 10, '*')} />,
+  document.querySelector('#react-root')
+);
